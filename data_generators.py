97df1a5f64486b500ab6063914e977e3fa790030
azharsultan <azhar.sultan@terraloupe.com>
2019-05-13 11:31:54 +0200

    added comments, refactored a bit

--- data_generators.py
+++ data_generators.py
@@ -25,6 +25,7 @@ def create_datagen(train_X):
     )
     data_generator.fit(train_X)
     return data_generator
+
 def create_valgen(train_X):
     data_generator = ImageDataGenerator(
         featurewise_center=True,
@@ -34,9 +35,11 @@ def create_valgen(train_X):
     data_generator.fit(train_X)
     return data_generator
 
-def get_train_val_gen(batch_size, random_labels=False, kaggle_test_set=False):
+def get_fashion_dataset(batch_size, random_labels=False, kaggle_test_set=False):
+    # get data
     (x, y), (x_test,y_test) = fashion_mnist.load_data()
 
+    # if testing kaggle version of fashion-mnist. path is hardcoded
     if kaggle_test_set:
         raw = pd.read_csv('data/fashionmnist/fashion-mnist_test.csv')
         y_test = raw.label
@@ -44,19 +47,36 @@ def get_train_val_gen(batch_size, random_labels=False, kaggle_test_set=False):
         x_as_array = raw.values[:,1:]
         x_test = x_as_array.reshape(num_images, 28, 28)
 
+    # add channels dimension and normalize the data by dividing with 255
     x = np.expand_dims(x,axis=-1)/255
     x_test = np.expand_dims(x_test,axis=-1)/255
+
+    # split data between training and validation
     x_train,x_val, y_train, y_val = train_test_split(x, y, test_size=VAL_SIZE, random_state=RANDOM_STATE)
+
+    # randomize labels if testing convergence of models against random labels
     if random_labels:
         np.random.shuffle(y_train)
+
+    # one-hot incoding
     y_train = to_categorical(y_train, num_classes=NUM_CLASSES)
     y_val = to_categorical(y_val, num_classes=NUM_CLASSES)
     y_test = to_categorical(y_test, num_classes=NUM_CLASSES)
+
+    # Data augmentation. validation augmentation is mainly about mean subtraction
     train_augmentation = create_datagen(x_train)
     val_augmentation = create_valgen(x_train)
 
+    # create training generator with given batch size
     train_gen = train_augmentation.flow(x_train,y_train, batch_size)
+
+    # create validation and test data after passing it through validation augmentation
+    # needed because of a bug in keras
     val_data = next(val_augmentation.flow(x_val,y_val, len(x_val), shuffle=False))
     test_data = next(val_augmentation.flow(x_test,y_test, len(x_test), shuffle=False))
 
     return train_gen, val_data, test_data
+
+def get_labels():
+    labels = ['t_shirt_top', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle_boots']
+    return labels
--- embeddings.py
+++ embeddings.py
@@ -9,10 +9,20 @@ from keras.backend import clear_session
 from tensorflow.contrib.tensorboard.plugins import projector
 
 from helpers import get_sprite_image
-from data_generators import get_train_val_gen
+from data_generators import get_fashion_dataset, get_labels
 
 
-def save_embeddings(features, metadata_path, sprite_path, outdir, model_name):
+def save_embeddings(features,outdir, model_name):
+    """
+    Fuction to save embeddings data for tensorboard
+    Args:
+        features: data to use as embedding
+        outdir: output directory
+        model_name: name of the embeddings file being saved
+
+    Returns:
+
+    """
     tf.reset_default_graph()
     embedding_var = tf.Variable(features, name='embeddings')
     # Format: tensorflow/tensorboard/plugins/projector/projector_config.proto
@@ -22,8 +32,9 @@ def save_embeddings(features, metadata_path, sprite_path, outdir, model_name):
     embedding = config.embeddings.add()
     embedding.tensor_name = embedding_var.name
     # Link this tensor to its metadata file (e.g. labels).
-    embedding.metadata_path = metadata_path
-    embedding.sprite.image_path = sprite_path
+    # path to theses files needs to be relative to the output directory
+    embedding.metadata_path = '../metadata.tsv'
+    embedding.sprite.image_path = '../fashion-mnist-sprite.png'
     # Specify the width and height of a single thumbnail.
     embedding.sprite.single_image_dim.extend([28, 28])
 
@@ -37,39 +48,58 @@ def save_embeddings(features, metadata_path, sprite_path, outdir, model_name):
         sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver()
         saver.save(sess, os.path.join(outdir, '%s.ckpt'%(model_name)))
-        summary_writer.close()
-        #sess.close()
-        #del sess,summary_writer, saver
 
 def save_embeddings_for_training(base_path, outdir):
-    labels = ['t_shirt_top', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle_boots']
+    """
+    Function that searches for all model snapshot in base_path and creates embeddings for them. It extracts features
+    from third last layer of the model for embeddings.
+    Args:
+        base_path: Directory where to look for model snapshots
+        outdir: output directory where embeddings are saved. Embeddings for each model are saved in a separate folder
+
+    Returns:
+        None
+    """
+    # look for model snapshots in the base_path
     model_paths = glob(base_path+'*.h5')
-    print(model_paths)
+
     sprite_path = os.path.join(outdir,'fashion-mnist-sprite.png')
     metadata_path = os.path.join(outdir,'metadata.tsv')
 
-    _, (x_val, y_val), _ = get_train_val_gen(batch_size=32)
+    # get fashion-mnist data
+    _, (x_val, y_val), _ = get_fashion_dataset(batch_size=32)
+    # convert normalized images back to 0-255 range (to create sprite image)
     x_val_images = (x_val - np.min(x_val)) * 255
+    # convert one-hot-encoded labels back to values
     y_val = np.argmax(y_val, axis=-1)
 
     if not os.path.exists(base_path):
+        # save labels as class names in metadata
+        labels = get_labels()
         y_str = np.array([labels[j] for j in y_val])
         np.savetxt(metadata_path, y_str, fmt='%s')
 
     if not os.path.exists(sprite_path):
+        # save sprite image
         plt.imsave(sprite_path, get_sprite_image(x_val_images), cmap='gray')
         exit()
 
+    # save embeddings in a separate folder for each path
     for model_path in model_paths:
+        # needed to avoided retaining previous embeddings data in the for loop
         clear_session()
+
+        # create new folder based on model snapshot name
+        model_id = model_path.split('/')[-1][:-3]
+        embedding_dir = os.path.join(outdir, model_id)
+        os.makedirs(embedding_dir, exist_ok=True)
+
+        # extract features from third last layer of the model
         model = load_model(model_path)
         model = Model(model.input, model.layers[-3].output)
         features = model.predict(x_val)
-        model_id = model_path.split('/')[-1][:-3]
-        embedding_dir = os.path.join(outdir,model_id)
-        os.makedirs(embedding_dir, exist_ok=True)
-        print("passing embeddings to save")
-        save_embeddings(features,metadata_path,sprite_path,embedding_dir,model_id)
+
+        save_embeddings(features,embedding_dir,model_id)
 
 @click.command()
 @click.argument('base_path')
